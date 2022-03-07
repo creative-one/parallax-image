f37c349c680826d51f4b659f12bdf4574070410e
mz@360vier.de <mz@360vier.de>
2022-03-07 10:07:07 +0100

    Initial commit

---
+++ parallax-image.js
@@ -0,0 +1,34 @@
+function parallax_scroll_handler (image) {
+    //const scrollTop = window.scrollTop || window.scrollY
+    const imgTag = image.querySelector('img.parallax-image')
+    const height = imgTag.getAttribute('data-height')
+    const speed = parseFloat(imgTag.getAttribute('data-speed'))
+    const rect = image.getBoundingClientRect()
+    const imagePos = rect.top
+    const winH = window.innerHeight
+    const scrollPos = imagePos
+
+    const imgHeight = `calc((${winH}px - ${height}) * ${speed} + ${height})`
+
+    imgTag.style.transform = `translateY(${scrollPos * -speed}px)`
+    imgTag.style.height = `${imgHeight}`
+}
+
+function init_parallax () {
+    const imageDoms = document.querySelectorAll('img.parallax-image')
+    const images = Array.from(imageDoms)
+
+    images.forEach(image => {
+        const imageEl = document.createElement('div')
+        imageEl.className = 'parallax-image-wrapper'
+        imageEl.innerHTML = image.outerHTML
+        imageEl.style.height = image.getAttribute('data-height')
+        image.replaceWith(imageEl)
+        parallax_scroll_handler(imageEl)
+        window.addEventListener('scroll', () => parallax_scroll_handler(imageEl))
+    })
+}
+
+(function() {
+    init_parallax()
+})();
